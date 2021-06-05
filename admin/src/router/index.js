9174c8572eef12207767f050cbf2817db8d8a429
Chriszhifeng <644191901@qq.com>
2021-06-05 17:21:52 +0800

    update:admin/src/router/index.js

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,7 +1,17 @@
+/*
+ * @Author: Chris:)
+ * @Date: 2021-06-05 11:02:16
+ * @LastEditTime: 2021-06-05 17:21:06
+ * @LastEditors: Chris:)
+ * @FilePath: /admin-home-server/admin/src/router/index.js
+ */
 import { createRouter, createWebHashHistory } from 'vue-router'
 import home from '../views/home.vue'
 
 const routes = [
+   //路由级代码分割
+   //这会为此路由生成一个单独的块（about.[hash].js）
+   //这是在访问路由时延迟加载的。
   {
     path: '/',
     name: 'home',
